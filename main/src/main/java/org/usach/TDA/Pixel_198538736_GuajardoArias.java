bb2bd31bbd4ce0e06f5063007c58cb35ec002ba2
Ivan Guajardo Arias <ivan.guajardo.a@usach.cl>
2022-11-29 23:29:25 -0300

    Terminé de comentar pix

--- main/src/main/java/org/usach/TDA/Pixel_198538736_GuajardoArias.java
+++ main/src/main/java/org/usach/TDA/Pixel_198538736_GuajardoArias.java
@@ -1,15 +1,25 @@
 package org.usach.TDA;
 
+
+/*
+Clase que representa los pixeles, contiene valores para los 3 tipos e implementa métodos para manipularlos adecuadamente en cada caso
+ */
 public class Pixel_198538736_GuajardoArias{
 
+    // atributos de la clase
+    // x e y representan las coordenadas por medio de enteros
     private int x;
 
     private int y;
 
+    // d representa la profundidad del pixel
     private int d;
 
+    // color contendrá un objeto de tipo color, que representa la información del color para el pixel
     private Color_198538736_GuajardoArias color;
 
+
+    // constructor de pixel
     public Pixel_198538736_GuajardoArias(int x, int y, int d, Color_198538736_GuajardoArias color) {
         this.x = x;
         this.y = y;
@@ -17,11 +27,12 @@ public class Pixel_198538736_GuajardoArias{
         this.color = color;
     }
 
-
+    // get color para poder obtener la información del color del pixel
     public Color_198538736_GuajardoArias getColor() {
         return this.color;
     }
 
+    // checkers para comprobar el tipo de pixel que representa en cada caso
     public boolean isBit(){
         return this.color.isBit();
     }
@@ -34,10 +45,12 @@ public class Pixel_198538736_GuajardoArias{
         return this.color.isRGB();
     }
 
+    // checker para comprobar si el pixel se encuentra dentro de las coordenadas dadas
     public boolean inRange(int x1, int y1, int x2, int y2){
         return (this.x >= x1) && (this.x <= x2) && (this.y >= y1) && (this.y <= y2);
     }
 
+    // setters para las coordenadas x e y
     public void setX(int x) {
         this.x = x;
     }
@@ -46,11 +59,15 @@ public class Pixel_198538736_GuajardoArias{
         this.y = y;
     }
 
+    // conversor de rgb a hex que llama al conversor dentro del color y construye
+    // un nuevo pixel con el hex obtenido
     public Pixel_198538736_GuajardoArias pixRGBToHex() {
         Color_198538736_GuajardoArias C = this.color.rgbToHex();
         return new Pixel_198538736_GuajardoArias(this.x, this.y, this.d, C);
     }
 
+    // para intercambiar las coordenadsa del pixel y utilizarlo posteriormente al transponer
+    // la lista
     public Pixel_198538736_GuajardoArias swapPositions(){
         int newX = this.y;
         int newY = this.x;
@@ -63,6 +80,8 @@ public class Pixel_198538736_GuajardoArias{
     }
 
 
+    // métodos para mover el pixel de acuerdo al ancho y largo
+    // y así poder realizar los flips
     public void move_pix_h(int width) {
         int width1 = width-1-this.x;
         setX(width1);
@@ -73,6 +92,7 @@ public class Pixel_198538736_GuajardoArias{
         setY(height1);
     }
 
+    // printPix permite imprimir la información del pixel de manera ordenada
     public void printPix(){
         System.out.println("X: " + this.x + " " + "Y: " + this.y + " " + this.color.toString() + " " + "Depth: " + this.d + "\n");
     }
